5030af2816a6939a3d99185a2902363fa0b8d436
Alisha Humagain <alishahumagain516@gmail.com>
2025-01-24 09:45:00 -0500

    Add lab 03 code

--- lab03.py
+++ lab03.py
@@ -1,1 +1,8 @@
-print("Hello, World")
+#Dice options using list and range()
+diceOptions = list(range(1,7))
+
+#Weapons Array
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "NuclearBomb"]
+
+#Display available weapons
+print("Available Weapons:")
